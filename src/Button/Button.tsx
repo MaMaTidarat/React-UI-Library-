6366d3a56c4a27f2524a17e886c0178845456b8f
Tidarat <tidarat_srik@truecorp.co.th>
2021-05-18 21:03:51 +0700

    feat: 🎸 Button component
    ✅ Closes: npx tailwindcss init

---
+++ src/Button/Button.tsx
@@ -0,0 +1,5 @@
+import React, { PropsWithChildren } from 'react';
+
+export const Button = ({ children }: PropsWithChildren<{}>) => (
+  <button className="px-2 py-1 outline-none bg-blue-500">{children}</button>
+);
