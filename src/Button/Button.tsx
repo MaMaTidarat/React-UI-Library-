ba16caf512258b77607b79aeb8779c4bc1324999
Tidarat <tidarat_srik@truecorp.co.th>
2021-05-28 16:30:33 +0700

    test

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,26 @@
 import React, { PropsWithChildren } from 'react';
 
-export const Button = ({ children }: PropsWithChildren<{}>) => (
-  <button className="px-2 py-1 outline-none bg-blue-500">{children}</button>
+export type ButtonProps = {
+  primary?: boolean;
+} & JSX.IntrinsicElements['button'];
+
+const baseClassName =
+  'bg-blue-500  hover:bg-blue-300 focus:ring-blue-400 focus:ring-opacity-75';
+const primaryClassName =
+  'bg-green-500  hover:bg-green-300 focus:ring-green-400 focus:ring-opacity-75';
+
+export const Button = ({
+  children,
+  primary,
+  className,
+  ...props
+}: PropsWithChildren<ButtonProps>) => (
+  <button
+    className={`py-2 px-4 font-semibold rounded-lg shadow-md  text-white ${
+      primary ? primaryClassName : baseClassName
+    }`}
+    {...props}
+  >
+    {children}
+  </button>
 );
